77ac0200fd66bfb54bf7672b4af731815db5681b
biancasilva <bianca@fanhero.com>
2024-02-07 14:24:31 -0300

    feat: add postcss.config.cjs and tailwind.config.js files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage/HomePage"
+import MainLayout from "./layouts/MainLayout/MainLayout"
+import { AuthProvider } from "./context/AuthContext"
+import ArtistForm from "./pages/ArtistForm/ArtistForm"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { queryClient } from "./api/config"
 
+/**
+ * The main component of the application.
+ * Renders the application layout and routes.
+ */
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-			</Routes>
-		</Router>
+		<AuthProvider>
+			<QueryClientProvider client={queryClient}>
+				<Router>
+					<Routes>
+						<Route path='/' element={<MainLayout />}>
+							<Route path='/' element={<HomePage />} />
+							<Route path='/c/artist' element={<ArtistForm />} />
+						</Route>
+					</Routes>
+				</Router>
+			</QueryClientProvider>
+		</AuthProvider>
 	)
 }
 
